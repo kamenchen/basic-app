34cc5f32ac11f34c78e11f9dac10d369df10ef79
Kamen Chen Yu <Kamen.ChenYu01@student.csulb.edu>
2025-09-11 14:30:39 -0700

    change 3

---
+++ src/app/users/newuser/newuser.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newuser',
+  templateUrl: './newuser.page.html',
+  styleUrls: ['./newuser.page.scss'],
+  standalone: false,
+})
+export class NewuserPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
   {
     path: 'detail',
     loadChildren: () => import('./detail/detail.module').then( m => m.DetailPageModule)
+  },
+  {
+    path: 'newuser',
+    loadChildren: () => import('./newuser/newuser.module').then( m => m.NewuserPageModule)
   }
 ];
 
